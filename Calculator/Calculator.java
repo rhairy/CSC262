0378fc8598b7bc063ab332f9ac81470b7c34baf1
rhairy <randall.harrison91@gmail.com>
2016-01-05 19:29:57 -0800

    got started

---
+++ Calculator/Calculator.java
@@ -0,0 +1,21 @@
+/* Calculator Application. */
+
+import java.util.Scanner;
+
+public class Calculator
+{
+	public static void main(String[] args)
+	{
+		/* Instantiate Scanner object to take standard input. */
+		Scanner inStream = new Scanner(System.in);
+
+		/* Define variables to hold input. */
+		String input;
+		String operator;
+
+		System.out.println("Press q at any time to quit.");
+
+		System.out.print("Enter the first value: ");
+		input = inStream.next();
+	}
+}
