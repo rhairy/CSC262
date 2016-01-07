48edcec311866be84588ccf1d1b255ca94aa55bf
rhairy <randall.harrison91@gmail.com>
2016-01-06 22:06:45 -0800

    Added basic GuessaTron implementation.

---
+++ GuessaTron/GuessaTron.java
@@ -0,0 +1,84 @@
+/**********************************************************/
+// Assignment :  GuessaTron
+// by: Randall Harrison
+// // Date: 2016-01-11
+// // Description: Program to guess a number from 1 to 100.
+// //  Notes: any special things you want me to consider.
+// //***********************************************************
+// //************************************************************
+// If there are any questions that need to be answered put them here as comments
+// */
+
+import java.util.Scanner;
+
+public class GuessaTron 
+{
+	public static boolean isValid(String command)
+	{
+		boolean result = false;
+
+		switch (command) {
+			case "H":
+				result = true;
+				break;
+			case "L":
+				result = true;
+				break;
+			case "Q":
+				result = true;
+				break;
+			case "C":
+				result = true;
+				break;
+			default:
+				System.out.printf("ERROR - %s is an invalid command.\n", command);
+				result = false;
+				break;
+		}
+		return result;
+	}
+
+	public static void main(String[] args)
+	{
+		int upper = 100;
+		int lower = 1;
+		int offer = 50;
+		
+		String input;
+
+		Scanner inStream = new Scanner(System.in);
+		
+		do {
+			do {
+				System.out.printf("Computer says %d. Is your number higher or lower?\n", offer);
+				input = inStream.next();
+			} while (!isValid(input));
+
+			switch (input) {
+				case "L": 
+					upper = offer;
+					offer = (upper + lower) / 2;
+					break;
+				case "H":
+					lower = offer;
+					offer = (upper + lower) / 2;
+					break;
+				case "Q":
+					System.out.println("YOU QUIT - Thanks for playing!");
+					return;
+				default:
+					break;
+			}
+			} while (!input.equals("C"));
+		System.out.printf("Your number was: %d\n", offer);	
+	}
+}
+ 								   
+
+ 								   	   
+ 								   	   	   
+ 								   	   	  
+
+///////////////////////////////////////////////////////////////////
+///   End Of File
+///////////////////////////////////////////////////////////////////
