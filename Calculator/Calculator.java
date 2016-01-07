003c3fefa7639f8515f4ec85b46893f0588bda5e
rhairy <randall.harrison91@gmail.com>
2016-01-06 20:12:16 -0800

    Working prototype

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -28,11 +28,14 @@ public class Calculator
 		String operator;
 		int number1;
 		int number2;
+		int result = 0;
 
-		System.out.println("Press q at any time to quit.");
+		System.out.println("This is Randy's calculator for Molly to do her math here at National University.");
+		System.out.println("This program is written by Randy Harrison.");
+		System.out.println("This calculator program does Addition, Subtraction, Multiplication, and Division.");
 
 		/* Get the first value. */
-		System.out.print("Enter the first value: ");
+		System.out.print("Enter your first value: ");
 		input = inStream.next();
 
 		if (input.equalsIgnoreCase("q")) {
@@ -47,9 +50,46 @@ public class Calculator
 		input = "";
 
 		/* Get the operator. */
-		System.out.println("Enter an operator: + - * /");
+		System.out.print("Enter your operator \"+ - * /\": ");
 		input = inStream.next();
 
 		/* Evaluate input here, maybe use a case statement? */
+		if (input.equalsIgnoreCase("q")) {
+			return;
+		} else if (input.equalsIgnoreCase("+") || input.equalsIgnoreCase("-") || input.equalsIgnoreCase("*") || input.equalsIgnoreCase("/")) {
+			operator = input;
+		} else {
+			System.out.println("ERROR - Invalid Operator.");
+			return;
+		}
+
+		/* Get the second value. */
+		System.out.print("Enter your second value: ");
+                input = inStream.next();
+ 
+                if (input.equalsIgnoreCase("q")) {
+                        return;
+                } else if (isInt(input)) {
+                        number2 = Integer.parseInt(input);
+                } else {
+                        System.out.println("Invalid input.");
+                        return;
+                }
+
+		/* Perform the calculation. */
+		switch (operator) {
+			case "+": result = number1 + number2;
+				break;
+			case "-": result = number1 - number2;
+				break;
+			case "*": result = number1 * number2;
+				break;
+			case "/": result = number1 / number2;
+				break;
+			default: System.out.println("ERROR - Could not calculate.");
+				break;
+		}
+		
+		System.out.printf("Your total of %d %s %d is %d.\n", number1, operator, number2, result);
 	}
 }
