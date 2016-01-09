15ab62d27a2d2c4766f5279b7dfce01d163bab38
rhairy <randall.harrison91@gmail.com>
2016-01-08 20:21:21 -0800

    Added methods.

---
+++ ParachuteDrop/ParachuteDrop.java
@@ -0,0 +1,41 @@
+// Assignment :  <name of assignment>
+// // by: Randall Harrison
+// // Date: 2016-01-11
+// // Description: Calculate speed of a parachuting object.
+// //  Notes: any special things you want me to consider.
+// If there are any questions that need to be answered put them here as comments
+//
+import java.util.Scanner;
+
+public class ParachuteDrop
+{
+	public static double parachuteDiameter(double mass, double velocity)
+	{
+			
+	}
+
+	public static double fpsToMps(double fps)
+	{
+		return fps * .3048;
+	}
+
+	public static double metersToFeet(double meters)
+	{
+		return meters * 3.2808;
+	}
+
+	public static double poundsToKilos(double ibs)
+	{
+		return ibs * 0.453592;
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
+
+// 	   ///////////////////////////////////////////////////////////////////
+// 	   ///   End Of File
+// 	   ///////////////////////////////////////////////////////////////////
