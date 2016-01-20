cd625c4f132e57956d8d2ee9ef46e31fd7cb20a7
rhairy <randall.harrison91@gmail.com>
2016-01-19 21:06:35 -0800

    Added basic implementations of methods.

--- SalesArrays/src/com/rhairy/salesarrays/MailOrders.java
+++ SalesArrays/src/com/rhairy/salesarrays/MailOrders.java
@@ -1,6 +1,40 @@
 package com.rhairy.salesarrays;
 
+import java.util.ArrayList;
+
 public class MailOrders
 {
+	// Private Instance Variables.
+	private ArrayList<MyInventory> inventory = new ArrayList<MyInventory>();
 	
+	// Public Methods.
+	
+	// Populates the ArrayList with MyInventory objects.
+	public void FillInventory( int ProductNum, double Price, int Qty )
+	{
+		// Instantiate a MyInventory object with the supplied parameters, and add it to the ArrayList.
+		MyInventory temp = new MyInventory();
+		
+		temp.setInventory( ProductNum, Price, Qty );
+		
+		inventory.add( temp );
+	}
+	
+	public MyInventory ProcessOrder( int ProductNum, int Qty )
+	{
+		return null;
+	}
+	
+	// Iterate through the ArrayList and return the MyInventory object with the provided ProductNum.
+	public MyInventory getInventoryItem( int ProductNum )
+	{
+		for ( MyInventory i : inventory ) {
+			if ( i.getProductNum() == ProductNum ) {
+				return i;
+			} else {
+				continue;
+			}
+		}
+		return null;
+	}
 }
--- SalesArrays/src/com/rhairy/salesarrays/MyInventory.java
+++ SalesArrays/src/com/rhairy/salesarrays/MyInventory.java
@@ -8,28 +8,36 @@ public class MyInventory
 	private int Qty;
 	
 	// Public Methods.
-	public int setInventory( int ProductNum, double Price, int Qty )
+	
+	// Set the ProductNum, Price, and Qty for a MyInventory object.
+	public void setInventory( int ProductNum, double Price, int Qty )
 	{
-		
+		this.ProductNum = ProductNum;
+		this.Price = Price;
+		this.Qty = Qty;
 	}
 	
+	// Accessor: Return the ProductNum.
 	public int getProductNum()
 	{
-		
+		return this.ProductNum;
 	}
 	
+	// Accessor: Return the Price.
 	public double getPrice()
 	{
-		
+		return this.Price;
 	}
 	
+	// Accessor: Return the qty.
 	public int getQty()
 	{
-		
+		return this.Qty;
 	}
 	
-	public int UpdateQty( int num )
+	// Mutator: Change the qty of the object.
+	public void UpdateQty( int num )
 	{
-		
+		this.Qty = num;
 	}
 }
