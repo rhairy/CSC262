86da6b683f63a849854c496fb82c08f10f166510
rhairy <randall.harrison91@gmail.com>
2016-01-20 18:30:49 -0800

    Added basic working implentation to MailOrderTest.java

--- SalesArrays/src/com/rhairy/salesarrays/MailOrderTest.java
+++ SalesArrays/src/com/rhairy/salesarrays/MailOrderTest.java
@@ -1,23 +1,34 @@
 import com.rhairy.salesarrays.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MailOrderTest
 {
 	public static void main( String [] args )
 	{
+		// Introduce program.
+		System.out.println( "Welcome to the MailOrderTest program." );
+		System.out.println( "This program read an inventory list from a csv and simulate the purchasing of one item." );
+		
+		String filePath = "";
+		
+		// Get the file path from the user and instantiate a File object.
+		try ( Scanner inputScanner = new Scanner(System.in) ) {
+			System.out.print( "Enter the path to the inventory file (.csv): " );
+			filePath = inputScanner.next();
+		} 
+		
+		File inventoryFile = new File(filePath);
+		
+		// Instantiate MailOrders Object.
 		MailOrders myOrder = new MailOrders();
 		
-		myOrder.FillInventory( 1, 10, 1 );
-		myOrder.FillInventory( 2, 20, 2 );
-		
-		MyInventory product1 = myOrder.getInventoryItem( 1 );
-		
-		if ( product1 == null ) {
-			System.out.printf( "Could not find product.\n");
-		} else {
-			System.out.println( "The product was found!\n");
-			System.out.printf( "The product's ProductNum is: %d.\n", product1.getProductNum() );
-			System.out.printf( "The product's Price is: %f.\n", product1.getPrice() );
-			System.out.printf( "The Product's Qty is: %d.\n", product1.getQty() );
+		// Open csv file and add it to the MailOrder object as a MyInventory object.
+		try ( Scanner inventoryScanner = new Scanner(inventoryFile);) {
+			System.out.println( "Hello World!" );
+		} catch ( FileNotFoundException e) {
+			System.out.printf( "Could not find file: %s\n", filePath );
 		}
 	}
 }
